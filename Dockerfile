e63efa41bdd92cd304bc2efafb6352895bb5ce38
Pauline <paulinezhou2003@gmail.com>
2025-05-12 21:38:59 +0000

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,15 @@ FROM python:3.11-slim
 # Dossier de travail
 WORKDIR /app
 
-# Copier les fichiers
+# Copier les fichiers de dépendances
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
+# Copier l'application et le dossier utils
 COPY app.py .
+COPY utils /app/utils  
 
+# Exposer le port
 EXPOSE 8080
 
 # Commande pour lancer l'application
--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+import os
 from flask import Flask
+from datetime import datetime
+from flask import request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
+def health_check():
+    return {"status": "ok"}
+
+@app.route("/hello")
 def hello():
     return "Hello from cloud run ! 🚀"
 
+@app.route("/status", methods=["GET"])
+def status():
+    return {"status": "ok", "server_time": datetime.utcnow()}
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
