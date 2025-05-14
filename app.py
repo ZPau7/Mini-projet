65a389888247ecc348b4f4acd69060c925ccc2a0
thomas <thomas.cai@esme.fr>
2025-05-14 11:13:50 +0200

    J'ai ajouté la partie Get et Post

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 from datetime import datetime
 from flask import request, jsonify
+from utils.gcs_utils import read_data, append_data
+
 
 app = Flask(__name__)
 
@@ -17,6 +19,23 @@ def hello():
 @app.route("/status", methods=["GET"])
 def status():
     return {"status": "ok", "server_time": datetime.utcnow()}
+
+@app.route("/data", methods=["GET"])
+def get_data():
+    try:
+        data = read_data()
+        return jsonify(data)
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+@app.route("/data", methods=["POST"])
+def post_data():
+    try:
+        new_row = request.get_json()
+        append_data(new_row)
+        return {"message": "Data added successfully"}
+    except Exception as e:
+        return {"error": str(e)}, 500
 
 
 if __name__ == "__main__":
